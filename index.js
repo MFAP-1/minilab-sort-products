fe51bd59f9e15096fea756ae4e5f63d074c3626b
Manoel Pereira <manoelpereiraeng@gmail.com>
2021-08-11 15:39:35 -0300

    1st commit. 'MINILAB | Filter Products' solved.

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-// Seu código aqui!
+// WEEK 1 - DAY 2: MINILAB | Filter Products [MFAP-1]
 
+// given array of objects
 const products = [
   {
     id: 1,
     name: 'Iphone 11',
     tags: ['smartphone', 'tech', 'apple']
   },
-    {
+  {
     id: 2,
     name: 'Macbook Pro',
     tags: ['laptop', 'tech', 'apple']
   },
-    {
+  {
     id: 3,
     name: 'JBL Headphones',
     tags: ['headphone', 'tech', 'jbl']
   },
-    {
+  {
     id: 4,
     name: 'Nike Shorts',
     tags: ['shorts', 'clothing', 'nike']
   },
 ];
 
-// Declare sua funçao aqui. Nao se esqueça de invocar a função para conseguir testar!
+// Function definition 
+const filterProducts = category => {
+  const filteredArr = [];
+  for (let i = 0; i < products.length; i++) {
+    for (let j = 0; j < products[i].tags.length; j++) {
+      if (products[i].tags[j] === category){
+        filteredArr.push(products[i]);
+        break; // increase efficiency. Reducing the number of loops when found.
+      }
+    }
+  }
+  return filteredArr;
+}
+
+// Calling the function and printing the result
+console.log(filterProducts('apple'));
